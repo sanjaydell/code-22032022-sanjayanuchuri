d2343eda1bf64e9c32aea6ad111390e2228bfce6
Sanjay Anuchuri <sanjay.anuchuri@gmail.com>
2022-03-21 20:19:05 +0530

    Created Content for Explore

--- src/components/Explore/Explore.jsx
+++ src/components/Explore/Explore.jsx
@@ -3,7 +3,31 @@ import './Explore.css'
 
 function Explore() {
   return (
-    <div className='Explore' id='Explore'>Explore</div>
+    <>
+      <div className='Explore' id='Explore'>
+        <div className='header'>
+          <h1>Explore</h1>
+          <h2>from one-guest rooms to</h2>
+          <h2>penthouses with pools and gardens</h2>
+        </div>
+        <div className='card-background'>
+          <div className='card' style={{
+              backgroundImage: 'url(https://stylesatlife.com/wp-content/uploads/2020/01/contemporary-bedroom-interior-design.jpg)'
+            }}>
+            <div className='description'>
+              sanjay
+            </div>
+          </div>
+          <div className='card' style={{
+              backgroundImage: 'url(https://stylesatlife.com/wp-content/uploads/2020/01/contemporary-bedroom-interior-design.jpg)'
+            }}>
+            <div className='description'>
+              sanjay
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
