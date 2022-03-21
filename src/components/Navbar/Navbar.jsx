c9a637cc647d5e13d0e274933efe99d25e53d0b4
Sanjay Anuchuri <sanjay.anuchuri@gmail.com>
2022-03-21 23:44:56 +0530

    Updated Navbar for Multiple Rendering

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import NavbarContents from './NavbarContents'
 import './Navbar.css'
 
-function Navbar() {
+function Navbar({color}) {
   return (
     <div className='Navbar'>
       <div className='left'>logo</div>
-      <div className='center'><NavbarContents /></div>
+      <div className='center'><NavbarContents color={color} /></div>
       {/* <div className='right'>menu</div> */}
     </div>
   )
--- src/components/Navbar/NavbarContents.jsx
+++ src/components/Navbar/NavbarContents.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
-const NavbarContents = () => {
+const NavbarContents = ({color}) => {
+  console.log(color)
+
   return (
     <>
-      <a href='#Explore' className='options'>Explore</a>
-      <a href='#About' className='options'>About Us</a>
-      <a className='options'>Cities</a>
-      <a className='options' style={{ borderStyle: 'solid', padding: '0.5rem 3.5rem', borderRadius: '20px'}}>Call</a>
+      <a href='#Explore' className='options' style={{color: `${color}`}}>Explore</a>
+      <a href='#About' className='options' style={{color: `${color}`}}>About Us</a>
+      <a className='options' style={{color: `${color}`}}>Cities</a>
+      <a className='options' style={{ borderStyle: 'solid', padding: '0.5rem 3.5rem', borderRadius: '20px', color: `${color}`}}>Call</a>
     </>
   )
 }
